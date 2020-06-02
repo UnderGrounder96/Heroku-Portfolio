e7c9c05a4c40d9949bb896467c84728cadffa443
Lucio Afonso <36516559+UnderGrounder96@users.noreply.github.com>
2020-06-02 18:12:45 +0200

    Update index.js

--- src/sections/hero/index.js
+++ src/sections/hero/index.js
@@ -35,7 +35,7 @@ class Hero extends React.Component {
                 loop: true
               }}/>
               <button className="hover-button"
-                onClick={()=>window.open("https://bit.ly/3diJ7eq")}>
+                onClick={()=>window.open("https://bit.ly/2U39BZX")}>
                 <span>CV Download</span>
               </button>
             </div>
